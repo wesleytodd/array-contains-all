440a90034503967a32be1bfc2ad3c7aedeae2d2c
Wes Todd <wes@wesleytodd.com>
2015-12-27 14:26:09 -0600

    fixed empty first array causing false positive

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function arrayContainsAll(a, b) {
-	return b.map(function(v){
+	return !!a.length && b.map(function(v){
 		return a.indexOf(v) !== -1;
 	}).reduce(function(v, v2) {
 		return v && v2;
--- test/index.js
+++ test/index.js
@@ -5,15 +5,18 @@ describe('arrayContainsAll', function() {
 	var a = [1, 2, 3];
 	var b = [1, 2];
 	var c = [1];
+	var d = [];
 
 	it('should retrun true if one array caontains all the entries of the other', function() {
 		assert(arrayContainsAll(a, a));
 		assert(arrayContainsAll(a, b));
 		assert(arrayContainsAll(a, c));
+		assert(arrayContainsAll(a, d));
 	});
 
 	it('should retrun false if one array does not caontain all the entries of the other', function() {
 		assert(!arrayContainsAll(b, a));
 		assert(!arrayContainsAll(c, a));
+		assert(!arrayContainsAll(d, a));
 	});
 });
