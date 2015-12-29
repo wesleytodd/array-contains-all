3a470b81fb284cac3b0671254e153e6e6ffc90b8
Wes Todd <wes@wesleytodd.com>
2015-12-28 23:09:11 -0600

    added to happiness

--- test/index.js
+++ test/index.js
@@ -1,20 +1,21 @@
+/* global describe, it */
 var arrayContainsAll = require('../');
 var assert = require('assert');
 
-describe('arrayContainsAll', function() {
+describe('arrayContainsAll', function () {
 	var a = [1, 2, 3];
 	var b = [1, 2];
 	var c = [1];
 	var d = [];
 
-	it('should retrun true if one array caontains all the entries of the other', function() {
+	it('should retrun true if one array caontains all the entries of the other', function () {
 		assert(arrayContainsAll(a, a));
 		assert(arrayContainsAll(a, b));
 		assert(arrayContainsAll(a, c));
 		assert(arrayContainsAll(a, d));
 	});
 
-	it('should retrun false if one array does not caontain all the entries of the other', function() {
+	it('should retrun false if one array does not caontain all the entries of the other', function () {
 		assert(!arrayContainsAll(b, a));
 		assert(!arrayContainsAll(c, a));
 		assert(!arrayContainsAll(d, a));
